a4e98dc6b7a9aa4a3a98fd965bde345a0cb229da
lopjf <lorispjf@gmail.com>
2023-12-10 17:16:24 +0000

    fix moves wasd

--- window/move.c
+++ window/move.c
@@ -4,23 +4,23 @@ static int check_next_pos(int keycode, int x, int y)
 {
 	if (keycode == KEY_W)
 	{
-		x = (int) (get_dda()->pos_x + get_dda()->dir_x);
-		y = (int) (get_dda()->pos_y + get_dda()->dir_y);
-	}
-	else if (keycode == KEY_S)
-	{
 		x = (int) (get_dda()->pos_x - get_dda()->dir_x);
 		y = (int) (get_dda()->pos_y - get_dda()->dir_y);
 	}
-	else if (keycode == KEY_D)
+	else if (keycode == KEY_S)
 	{
-		x = (int) (get_dda()->pos_x + get_dda()->plane_x);
-		y = (int) (get_dda()->pos_y + get_dda()->plane_y);
+		x = (int) (get_dda()->pos_x + get_dda()->dir_x);
+		y = (int) (get_dda()->pos_y + get_dda()->dir_y);
 	}
-	else if (keycode == KEY_A)
+	else if (keycode == KEY_D)
 	{
 		x = (int) (get_dda()->pos_x - get_dda()->plane_x);
 		y = (int) (get_dda()->pos_y - get_dda()->plane_y);
+	}
+	else if (keycode == KEY_A)
+	{
+		x = (int) (get_dda()->pos_x + get_dda()->plane_x);
+		y = (int) (get_dda()->pos_y + get_dda()->plane_y);
 	}
 	// printf("get_dda()->pos_x: %f, get_dda()->pos_y: %f\n", get_dda()->pos_x, get_dda()->pos_y);
 	// printf("x: %d, y: %d\n", x, y);
@@ -35,23 +35,23 @@ void update_pos(int keycode)
 		return;
 	if (keycode == KEY_W)
 	{
-		get_dda()->pos_x += get_dda()->dir_x;
-		get_dda()->pos_y += get_dda()->dir_y;
-	}
-	else if (keycode == KEY_S)
-	{
 		get_dda()->pos_x -= get_dda()->dir_x;
 		get_dda()->pos_y -= get_dda()->dir_y;
 	}
-	else if (keycode == KEY_D)
+	else if (keycode == KEY_S)
 	{
-		get_dda()->pos_x += get_dda()->plane_x;
-		get_dda()->pos_y += get_dda()->plane_y;
+		get_dda()->pos_x += get_dda()->dir_x;
+		get_dda()->pos_y += get_dda()->dir_y;
 	}
-	else if (keycode == KEY_A)
+	else if (keycode == KEY_D)
 	{
 		get_dda()->pos_x -= get_dda()->plane_x;
 		get_dda()->pos_y -= get_dda()->plane_y;
+	}
+	else if (keycode == KEY_A)
+	{
+		get_dda()->pos_x += get_dda()->plane_x;
+		get_dda()->pos_y += get_dda()->plane_y;
 	}
 }
 
