b55523b6f34629a216a84d7b5c8f8bdae2041c78
lopjf <lorispjf@gmail.com>
2023-12-15 19:37:59 +0000

    norm colorhandler

--- window/colorhandler.c
+++ window/colorhandler.c
@@ -6,38 +6,39 @@
 /*   By: loris <loris@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 19:18:31 by loris             #+#    #+#             */
-/*   Updated: 2023/12/15 19:20:10 by loris            ###   ########.fr       */
+/*   Updated: 2023/12/15 19:37:30 by loris            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cube.h"
 
+// utils is bpp, size_line and endian as per mlx_get_data_addr documentation
 int	get_color_tex(char wall_side, int x, int y)
 {
 	unsigned int	*img_data;
 	unsigned int	color;
-	int				bpp;
-	int				size_line;
-	int				endian;
-	int				r;
-	int				g;
-	int				b;
+	int				utils[3];
+	int				rgb[3];
 
 	if (wall_side == 'W')
-		img_data = (unsigned int *)mlx_get_data_addr(getb()->libx.we, &bpp, &size_line, &endian);
+		img_data = (unsigned int *)mlx_get_data_addr(getb()->libx.we, \
+	&utils[0], &utils[1], &utils[2]);
 	else if (wall_side == 'E')
-		img_data = (unsigned int *)mlx_get_data_addr(getb()->libx.ea, &bpp, &size_line, &endian);
+		img_data = (unsigned int *)mlx_get_data_addr(getb()->libx.ea, \
+	&utils[0], &utils[1], &utils[2]);
 	else if (wall_side == 'S')
-		img_data = (unsigned int *)mlx_get_data_addr(getb()->libx.so, &bpp, &size_line, &endian);
+		img_data = (unsigned int *)mlx_get_data_addr(getb()->libx.so, \
+	&utils[0], &utils[1], &utils[2]);
 	else if (wall_side == 'N')
-		img_data = (unsigned int *)mlx_get_data_addr(getb()->libx.no, &bpp, &size_line, &endian);
+		img_data = (unsigned int *)mlx_get_data_addr(getb()->libx.no, \
+	&utils[0], &utils[1], &utils[2]);
 	else
 		return (0 << 24 | 255 << 16 | 255 << 8 | 255);
-	color = img_data[y * size_line / 4 + x];
-	r = (color >> 16) & 0xFF;
-	g = (color >> 8) & 0xFF;
-	b = color & 0xFF;
-	return (0 << 24 | r << 16 | g << 8 | b);
+	color = img_data[y * utils[1] / 4 + x];
+	rgb[0] = (color >> 16) & 0xFF;
+	rgb[1] = (color >> 8) & 0xFF;
+	rgb[2] = color & 0xFF;
+	return (0 << 24 | rgb[0] << 16 | rgb[1] << 8 | rgb[2]);
 }
 
 int	get_rgb(char *rgb)
